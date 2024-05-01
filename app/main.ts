847f67a658740098913335854502854772293418
saurabhlm10 <saurabhlm10@gmail.com>
2024-05-01 20:22:13 +0530

    pass stage

--- app/main.ts
+++ app/main.ts
@@ -6,6 +6,17 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
+  console.log(
+    `[server] connected client: ${JSON.stringify(connection.address())}`
+  );
+
+  connection.on("data", () => {
+    connection.write("+PONG\r\n");
+  });
+
+  connection.end("end", () => {
+    console.log("client disconencted");
+  });
 });
 
 server.listen(6379, "127.0.0.1");
