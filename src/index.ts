5e481b94d3c606b08e05e7e1e24b2fed0ad5794c
saurabhlm10 <saurabhlm10@gmail.com>
2024-12-21 16:33:46 +0530

    refactor: code structure

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 import * as net from "net";
+import { parse } from "./utils/parse";
+import { serialazeSimpleError, serializeSimpleString } from "./utils/serialize";
+
+const commands = {
+  PING: () => serializeSimpleString("PONG"),
+};
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
   connection.on("data", (data) => {
-    connection.write("+PONG\r\n");
+    const [input] = parse(data);
+
+    switch ((input[0] as string).toUpperCase()) {
+      case "PING":
+        connection.write(commands.PING());
+        break;
+      default:
+        // throw Error(`Unknown command: ${input[0]}`);
+        connection.write(serialazeSimpleError(`Unknown command: ${input[0]}`));
+    }
   });
 });
 
