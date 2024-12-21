03ee34fdba78af5bc783c77eaa37efd7038cc01a
saurabhlm10 <saurabhlm10@gmail.com>
2024-12-21 16:32:31 +0530

    feat: serializer

---
+++ src/utils/serialize.ts
@@ -0,0 +1,16 @@
+export function serializeSimpleString(value: string) {
+  const str = `+${value}\r\n`;
+
+  return new Uint8Array(Buffer.from(str));
+}
+
+export function serializeBulkString(value: string) {
+  const str = `$${value.length}\r\n${value}\r\n`;
+
+  return new Uint8Array(Buffer.from(str));
+}
+
+export function serialazeSimpleError(value: string) {
+  const str = `-${value}\r\n`;
+  return new Uint8Array(new Buffer(str));
+}
